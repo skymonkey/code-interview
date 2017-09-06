2c0541d066199c745095ecde66c4282c42b6f027
Stephen Wilson <stephen.martin.wison@gmail.com>
2017-09-06 12:49:01 +0200

    Function to compute the sum of all multiples of two numbers below a certain limit.

---
+++ sum.multiples.R
@@ -0,0 +1,15 @@
+sum.multiples <- function(x, y, limit = 1000){
+# Calculate the sum of all multiples of two numbers
+# below a certain limit.
+#
+# Args:
+#  x, y:  the two numbers.
+#  limit: the upper multiple limit.
+#
+# Returns:
+#  The sum of all multiples of x and y which are < limit.
+
+  z <- seq(1:(limit - 1))
+  sum(z[z %% x == 0], z[z %% y == 0])
+
+}
